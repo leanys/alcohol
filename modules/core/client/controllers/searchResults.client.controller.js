d591fb048d87735c033137ed74fdfb7b74324145
Unknown <leanys@ufl.edu>
2019-03-19 14:45:26 -0400

    Plan B if modal doesnt work

--- modules/core/client/controllers/searchResults.client.controller.js
+++ modules/core/client/controllers/searchResults.client.controller.js
@@ -13,10 +13,16 @@
         vm.records = [];
         var len = $stateParams['drinkArray'].length;
         var drinkArray = $stateParams['drinkArray'];
+        vm.display = display;
               
         for(var i = 0; i < len; i++)
         {
-            vm.records[i] = {name: drinkArray[i].name, src: drinkArray[i].img, recipe: drinkArray[i].rec};
+            vm.records[i] = {name: drinkArray[i].name, src: drinkArray[i].img, 
+                recipe: drinkArray[i].rec, ingredients: drinkArray[i].ingredients};
+        }
+        
+        function display(){
+            console.log("okie dokie");
         }
     }
   }());
