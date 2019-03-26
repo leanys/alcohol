6f7b1c8f79bcc979501fb7cf14c3c86a95256b54
Unknown <shelbywarnock@ufl.edu>
2019-03-26 16:04:50 -0400

    Zing chart addition

--- modules/BAC/client/BAC.client.module.js
+++ modules/BAC/client/BAC.client.module.js
@@ -1,6 +1,7 @@
 (function (app) {
     'use strict';
   
-    app.registerModule('BAC', ['core']);
+    app.registerModule('BAC', ['core','zingchart-angularjs']);
     app.registerModule('BAC.routes', ['core.routes']);
+    //app.registerModule('zingchart', ['zingchart-angularjs']);
   }(ApplicationConfiguration));
--- modules/BAC/client/controllers/BAC.client.controller.js
+++ modules/BAC/client/controllers/BAC.client.controller.js
@@ -1,20 +1,133 @@
 (function () {
   'use strict';
-  var app = angular.module("BAC", []);
+  //var app = angular.module("BAC", ['zingchart-angularjs']);
   angular
     .module('BAC')
     .controller('BACcontroller', BACcontroller); 
 
-  function BACcontroller() { 
+    BACcontroller.$inject = ['$scope'];
+
+  function BACcontroller($scope) { 
     var vm = this;
     vm.weight = '';
     vm.percent = ''; //TO DO: Validate Percent
-    vm.sum= '';
+    vm.sum= 0;
     vm.gender ='';
     var g = '';
     vm.calculate = calculate;
     vm.hours = '';
     vm.interpretation = "";
+
+    window.feed = function(callback) {
+      var tick = {};
+      tick.plot0 = vm.sum;
+      callback(JSON.stringify(tick));
+    };
+
+    $scope.myJson = {
+      type: "gauge",
+      globals: {
+        fontSize: 20,
+        fontFamily: "Helvetica Neue"
+        
+      },
+      plotarea:{
+        marginTop:70
+      },
+      plot:{
+        size:'100%',
+        valueBox: {
+          placement: 'center',
+          text:'%v', //default
+          fontSize:20,
+          color:"#51554C",
+          rules:[
+            {
+              rule: '%v <= .08',
+              text: '%v<br>Good to drive'
+            },
+            {
+              rule: '%v < .2 && %v > .08',
+              text: '%v<br>Do not drive',
+              
+            },
+            {
+              rule: '%v < .25 && %v > .2',
+              text: '%v<br>Slow down'
+            },
+            {
+              rule: '%v >  .25',
+              text: '%v<br>Potential Alcohol Poisoning'
+            }   
+          ]
+        }
+      },
+      tooltip:{
+        borderRadius:5
+      },
+      scaleR:{
+        aperture:150,
+        minValue:0,
+        maxValue:.3,
+        step:.01,
+        center:{
+          visible:false
+        },
+        tick:{
+          visible:false
+        },
+        item:{
+          offsetR:0,
+          rules:[
+            {
+              rule:'%i == 9',
+              offsetX:15
+            }
+          ]
+        },
+        labels:['0','','','','','','','','','.08','','','','','','','','','','','','.20','','','','','.25'],
+        ring:{
+          size:40,
+          rules:[
+            {
+              rule:'%v <= .08',
+              backgroundColor:'#68e866'
+            },
+            {
+              rule:'%v < .2 && %v > .08',
+              backgroundColor:'#FFFE62'
+            },
+            {
+              rule: '%v < .25 && %v >= .2',
+              backgroundColor:'#FBC438'
+            },
+            {
+              rule:'%v >= .25',
+              backgroundColor:'#FF5E4D'
+            }      
+          ]
+        }
+      },
+      refresh:{  
+          type:"feed",
+          transport:"js",
+          url:"feed()",
+          interval:1500,
+          resetTimeout:1000
+      },
+      series : [
+        {
+          values : [vm.sum], // starting value
+          backgroundColor:'black',
+          indicator:[10,10,10,10,0.75],
+          animation:{  
+            effect:2,
+            method:1,
+            sequence:2,
+            speed: 900
+        },
+        }]
+    };
 
     function calculate() {
       var weight = vm.weight;
@@ -53,7 +166,7 @@
 
       
        console.log(sum);
-       vm.sum = sum;
+       vm.sum = parseFloat(sum.toFixed(2));
       //alert(sum);
 
       if (sum < 0.08){
