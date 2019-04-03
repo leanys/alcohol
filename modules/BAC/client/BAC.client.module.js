4fb6bc1f0b5b815ac4379fcf5bf43076b4eeb7f0
Unknown <shelbywarnock@ufl.edu>
2019-04-03 10:45:28 -0400

    Fixed modules

--- modules/BAC/client/BAC.client.module.js
+++ modules/BAC/client/BAC.client.module.js
@@ -1,8 +1,11 @@
 (function (app) {
     'use strict';
   
-
-    app.registerModule('BAC', ['core']);
-    app.registerModule('BAC.routes', ['ui.router', 'core.routes']);
+ 
+    app.registerModule('BAC', ['core','zingchart-angularjs']);
+    app.registerModule('BAC.routes', ['core.routes']);
+    //app.registerModule('zingchart', ['zingchart-angularjs']);
+    /*app.registerModule('BAC', ['core']);
+    app.registerModule('BAC.routes', ['ui.router', 'core.routes']);*/
 
   }(ApplicationConfiguration));
