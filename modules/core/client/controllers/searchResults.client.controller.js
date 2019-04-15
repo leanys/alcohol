7a3fe75f749ea8455eb24973695d8291c3bbb57c
zpohl97 <zpohl97@ufl.edu>
2019-04-15 17:40:10 -0400

    null and no drink fixes

--- modules/core/client/controllers/searchResults.client.controller.js
+++ modules/core/client/controllers/searchResults.client.controller.js
@@ -15,6 +15,10 @@
         vm.drink = drink;
         var len = $stateParams['drinkArray'].length;
         var drinkArray = $stateParams['drinkArray'];
+        if(drinkArray = [])
+        {
+         //TODO $state.go
+        }
         vm.openModal = openModal;
         vm.authentication = Authentication;
 
